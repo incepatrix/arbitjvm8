7a0cb9da893132f66e1bb02acae96a8cfb5cbc8e
incepatrix <mintu.tiwari@gmail.com>
2015-07-28 22:16:48 -0400

    initial tests with sqlite

---
+++ MMTPro/tests/dbconnection/DBConnectionTest.java
@@ -0,0 +1,20 @@
+package dbconnection;
+
+import static org.junit.Assert.*;
+import java.sql.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DBConnectionTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {	
+		fail("Not yet implemented");
+	}
+
+}
